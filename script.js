262c524bd72d0324e9116d7d52015e70e07ed825
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-02-10 22:14:08 +0900

    Created a fucntion operate and added 3 global variables

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply (number1, number2){
 function divide (number1, number2){
     return number1 / number2;
 }
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate (number1, number2, operator){
+    switch(operator){
+        case "/":
+            divide(number1,number2);
+            break;
+        case "*":
+            multiply(number1,number2);
+            break;
+        case "-":
+            subtract(number1,number2);
+            break;
+        case "+":
+            add(number1,number2);
+            break;
+    }
+}
