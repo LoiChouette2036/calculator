61001ba7928ba94e57678a2320dd645d78eabf05
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-02-12 14:46:11 +0900

    added an event when we click on a button, it should display on the result dispay form (rectangle)

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ function operate (number1, number2, operator){
             console.log("it's not working");
     }
 }
+
+const resultDisplay = document.getElementById("display");//select element that display result (the rectangle)
+
+const btns = document.querySelectorAll("button");
+btns.forEach (btn => {
+    btn.addEventListener('click', (event) => {
+        const buttonValue = event.target.textContent; //it take the value of button in a string
+        const numericValue = parseInt(buttonValue,10);// it converts in a number
+
+        alert("Valeur numérique du bouton : " + numericValue);
+        resultDisplay.textContent = numericValue;
+    })
+})
